5e547625a894238ec0bca73c7f5b20d4f8f136ac
NazarYefimov <123975599+NazarYefimov@users.noreply.github.com>
2023-05-01 22:39:38 +0300

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,101 +3,87 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 class FizzBuzz {
     private int n;
-    private BlockingQueue<String> queue;
+    private BlockingQueue<String> queueA;
+    private BlockingQueue<String> queueB;
+    private BlockingQueue<String> queueC;
+    private BlockingQueue<Integer> queueD;
 
     public FizzBuzz(int n) {
         this.n = n;
-        this.queue = new LinkedBlockingQueue<>();
-        queue.add("1");
+        this.queueA = new LinkedBlockingQueue<>();
+        this.queueB = new LinkedBlockingQueue<>();
+        this.queueC = new LinkedBlockingQueue<>();
+        this.queueD = new LinkedBlockingQueue<>();
     }
 
-    public void fizz() throws InterruptedException {
+    public void fizz() {
         for (int i = 3; i <= n; i += 3) {
             if (i % 5 != 0) {
-                System.out.println("fizz");
-                queue.put("fizz");
+                queueA.add(Integer.toString(i));
             }
         }
+        queueA.add("done");
     }
 
-    public void buzz() throws InterruptedException {
+    public void buzz() {
         for (int i = 5; i <= n; i += 5) {
             if (i % 3 != 0) {
-                System.out.println("buzz");
-                queue.put("buzz");
+                queueB.add(Integer.toString(i));
             }
         }
+        queueB.add("done");
     }
 
-    public void fizzbuzz() throws InterruptedException {
+    public void fizzbuzz() {
         for (int i = 15; i <= n; i += 15) {
-            System.out.println("fizzbuzz");
-            queue.put("fizzbuzz");
+            queueC.add(Integer.toString(i));
         }
+        queueC.add("done");
     }
 
-    public void number() throws InterruptedException {
-        for (int i = 2; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                System.out.println(i);
-                queue.put(Integer.toString(i));
-            } else {
-                while (queue.size() == 0) {
-                    Thread.sleep(1);
-                }
-                System.out.println(queue.take());
+    public void number() {
+        for (int i = 1; i <= n; i++) {
+            String output = "";
+            if (i % 3 == 0) {
+                output += "fizz";
             }
+            if (i % 5 == 0) {
+                output += "buzz";
+            }
+            if (output.isEmpty()) {
+                output += Integer.toString(i);
+            }
+            try {
+                if (queueA.peek().equals(Integer.toString(i))) {
+                    output = "fizz";
+                    queueA.take();
+                }
+                if (queueB.peek().equals(Integer.toString(i))) {
+                    output = "buzz";
+                    queueB.take();
+                }
+                if (queueC.peek().equals(Integer.toString(i))) {
+                    output = "fizzbuzz";
+                    queueC.take();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(output);
         }
     }
 
     public static void main(String[] args) {
-        int n = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz(n);
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
 
-        Thread t1 = new Thread(() -> {
-            try {
-                fizzBuzz.fizz();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        Thread threadA = new Thread(() -> fizzBuzz.fizz());
+        Thread threadB = new Thread(() -> fizzBuzz.buzz());
+        Thread threadC = new Thread(() -> fizzBuzz.fizzbuzz());
+        Thread threadD = new Thread(() -> fizzBuzz.number());
 
-        Thread t2 = new Thread(() -> {
-            try {
-                fizzBuzz.buzz();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        Thread t3 = new Thread(() -> {
-            try {
-                fizzBuzz.fizzbuzz();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        Thread t4 = new Thread(() -> {
-            try {
-                fizzBuzz.number();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-
-        try {
-            t1.join();
-            t2.join();
-            t3.join();
-            t4.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
     }
 }

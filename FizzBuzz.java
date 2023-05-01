54c47019a8e259f2b9fbd6eabfb8dcb9e03b609c
NazarYefimov <123975599+NazarYefimov@users.noreply.github.com>
2023-05-01 21:10:22 +0300

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,44 +1,47 @@
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-class FizzBuz {
+class FizzBuzz {
     private int n;
     private BlockingQueue<String> queue;
 
-    public FizzBuz(int n) {
+    public FizzBuzz(int n) {
         this.n = n;
         this.queue = new LinkedBlockingQueue<>();
         queue.add("1");
     }
 
-    public void fizz(Runnable printFizz) throws InterruptedException {
+    public void fizz() throws InterruptedException {
         for (int i = 3; i <= n; i += 3) {
             if (i % 5 != 0) {
+                System.out.println("fizz");
                 queue.put("fizz");
             }
         }
     }
 
-    public void buzz(Runnable printBuzz) throws InterruptedException {
+    public void buzz() throws InterruptedException {
         for (int i = 5; i <= n; i += 5) {
             if (i % 3 != 0) {
+                System.out.println("buzz");
                 queue.put("buzz");
             }
         }
     }
 
-    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+    public void fizzbuzz() throws InterruptedException {
         for (int i = 15; i <= n; i += 15) {
+            System.out.println("fizzbuzz");
             queue.put("fizzbuzz");
         }
     }
 
-    public void number(Runnable printNumber) throws InterruptedException {
+    public void number() throws InterruptedException {
         for (int i = 2; i <= n; i++) {
             if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
                 queue.put(Integer.toString(i));
             } else {
-
                 while (queue.size() == 0) {
                     Thread.sleep(1);
                 }
@@ -49,11 +52,11 @@ class FizzBuz {
 
     public static void main(String[] args) {
         int n = 15;
-        FizzBuz fizzBuzz = new FizzBuz(n);
+        FizzBuzz fizzBuzz = new FizzBuzz(n);
 
         Thread t1 = new Thread(() -> {
             try {
-                fizzBuzz.fizz(() -> System.out.println("fizz"));
+                fizzBuzz.fizz();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -61,7 +64,7 @@ class FizzBuz {
 
         Thread t2 = new Thread(() -> {
             try {
-                fizzBuzz.buzz(() -> System.out.println("buzz"));
+                fizzBuzz.buzz();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -69,7 +72,7 @@ class FizzBuz {
 
         Thread t3 = new Thread(() -> {
             try {
-                fizzBuzz.fizzbuzz(() -> System.out.println("fizzbuzz"));
+                fizzBuzz.fizzbuzz();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -77,7 +80,7 @@ class FizzBuz {
 
         Thread t4 = new Thread(() -> {
             try {
-                fizzBuzz.number(() -> {});
+                fizzBuzz.number();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
